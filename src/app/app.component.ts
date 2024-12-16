2ab7dfb2d364e2f2e77a863690f0378a1a7d9dbb
Milos2824 <Milos.milivojevic2824@gmail.com>
2024-12-16 19:23:51 +0100

    add rasa actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { WebService } from './web.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { MessageModel } from './models/messages.model';
+import { RasaModel } from './models/rasa-model';
 
 @Component({
   selector: 'app-root',
@@ -11,38 +13,75 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   webService = WebService.getInstance()
   title = 'interakcija-covek-racunar-2024';
   year = new Date().getFullYear();
 
+  ngOnInit(): void {
+    if(!localStorage.getItem('messages')){
+      localStorage.setItem('messages', JSON.stringify([
+        { type: 'bot', text: 'How can I help you?' }
+      ]))
+    }
+
+    this.messages = JSON.parse(localStorage.getItem('messages')!)
+  }
+
   isChatVisible = false
   userMessage: string = ''
-  messages = [
-    { type: 'bot', text: 'How can I help you?' }
-  ]
+  messages: MessageModel[] = []
+
+  pushMessage(message: MessageModel){
+    // if(message.type == 'bot'){
+    //   for(let m of this.messages){
+    //     if(m.text == 'Thinking...' && m.type == 'bot'){
+    //       m.text = message.text
+    //     }
+    //   }
+    // }else{
+    //   this.messages.push(message)
+    // }
+
+    this.messages.push(message)
+
+    localStorage.setItem('messages', JSON.stringify(this.messages))
+  }
+
   toggleChat() {
     this.isChatVisible = !this.isChatVisible
   }
   sendMessage() {
     if (this.userMessage.trim()) {
-      this.messages.push({ type: 'user', text: this.userMessage })
-      const obs = this.webService.sendRasaMessage(this.userMessage)
+      const trimmedInput = this.userMessage
+
       this.userMessage = ''
-      obs.subscribe(rsp => {
-        if (rsp.length == 0) {
-          this.messages.push({
-            type: 'bot',
-            text: 'Sorry I did not understand your question.'
+
+      this.pushMessage({ type: 'user', text: trimmedInput })
+      this.pushMessage({ type: 'bot', text: 'Thinking...' })
+
+      this.webService.sendRasaMessage(trimmedInput)
+        .subscribe((rsp: RasaModel[]) => {
+          if (rsp.length == 0) {
+            this.pushMessage({
+              type: 'bot',
+              text: 'Sorry I did not understand your question.'
+            })
+            return
+          }
+          rsp.map(msg => msg.image ? `<img src="${msg.image}" width="200">` : msg.text).forEach(msg => {
+            this.pushMessage({
+              type: 'bot',
+              text: msg!
+            })
           })
-          return
-        }
-        rsp.map(msg => msg.image ? `<img src="${msg.image}">` : msg.text).forEach(msg => {
-          this.messages.push({
-            type: 'bot',
-            text: msg!
-          })
-        })
+        },
+          (err: HttpErrorResponse) => {
+            this.pushMessage({
+              type: 'bot',
+              text: 'Sorry, I am not available at the moment.'
+            })
+
       })
     }
   }
